6b457111519f488035402ec57788ecc807496ae4
codewitHK1 <kumarhk1002@gmail.com>
2024-01-27 10:58:29 +0530

    added verify token middleware

--- Routes/taskRouts.js
+++ Routes/taskRouts.js
@@ -7,11 +7,13 @@ const {
   deleteTask,
 } = require("../Controller/TaskController");
 
+const verifyToken = require("../middleware/authMiddleware");
+
 const router = express.Router();
 
-router.post("/add-task/:user_id", addNewTask);
+router.post("/add-task/:user_id", verifyToken, addNewTask);
 router.get("/get-tasks/:user_id", getAllTasks);
-router.put("/update-task/:id", updateTask);
-router.delete("/delete-task/:taskId", deleteTask);
+router.put("/update-task/:id", verifyToken, updateTask);
+router.delete("/delete-task/:taskId", verifyToken, deleteTask);
 
 module.exports = router;
--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,14 +1,18 @@
+const jwt = require("jsonwebtoken");
+
 function verifyToken(req, res, next) {
   const token = req.headers["authorization"];
 
   if (!token) {
-    return res.status(403).json({ error: "Access Forbidden" });
+    return res
+      .status(403)
+      .json({ message: "Access denied, Please Login Again" });
   }
 
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       console.error("Error verifying token:", err);
-      return res.status(401).json({ error: "Unauthorized" });
+      return res.status(401).json({ message: "Unauthorized" });
     }
 
     req.user = decoded;
